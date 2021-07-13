ebbd9a83521e6e8bbf8a673cc14dd7a144669c14
Maketsu100 <87020481+Maketsu100@users.noreply.github.com>
2021-07-13 12:04:35 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+function show () {
+    alert("Sabía que dirías que SÍ!! Soy Telible! 😍😍");
+}
+
+function show1 () {
+    document.getElementById("no").style.top=(Math.random()*300)+"px";
+    document.getElementById("no").style.left=(Math.random()*300)+"px";
+}
